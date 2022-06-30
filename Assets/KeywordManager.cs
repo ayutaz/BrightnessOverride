791dd81ff1555c07ce43364a6120342b36ea367a
ayutaz <ka1357amnbpdr@gmail.com>
2022-06-30 17:24:50 +0900

    feat: 1秒ごとに輝度が0.1ずつ増えていき、ループする表現に変更

--- Assets/KeywordManager.cs
+++ Assets/KeywordManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using UniRx;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 
@@ -12,43 +14,21 @@ public class KeywordManager : MonoBehaviour
     public GameObject Content;
 
     // Use this for initialization
-    void Start()
+    private void Start()
     {
-        keywords.Add("Off", () =>
-        {
-            this.BrightnessOverride.Brightness = 0f;
-        });
-
-        keywords.Add("On", () =>
-        {
-            this.BrightnessOverride.Brightness = 1.0f;
-        });
-
-        keywords.Add("Darker", () =>
-        {
-            this.BrightnessOverride.Brightness -= 0.1f;
-        });
-        
-        keywords.Add("Brighter", () =>
-        {
-            this.BrightnessOverride.Brightness += 0.1f;
-        });
-
-        keywords.Add("Hide", () =>
-        {
-            this.Content.SetActive(false);
-        });
-
-        keywords.Add("Show", () =>
-        {
-            this.Content.SetActive(true);
-        });
-
-        keywords.Add("Quit", () =>
-        {
-            Application.Quit();
-        });
-
+        this.BrightnessOverride.Brightness = 0f;
+        Observable.Interval(TimeSpan.FromSeconds(1f))
+            .Subscribe(_ =>
+            {
+                if (BrightnessOverride.Brightness < 1f)
+                {
+                    BrightnessOverride.Brightness += 0.1f;
+                }
+                else if (BrightnessOverride.Brightness == 1f)
+                {
+                    BrightnessOverride.Brightness = 0f;
+                }
+            }).AddTo(this);
         // Tell the KeywordRecognizer about our keywords.
         keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
 
